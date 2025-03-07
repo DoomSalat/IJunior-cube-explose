cb039cddc0f23038f3c8f276ac8be14795cc730a
Salatnicha <k.zaitsev@tushitesvet.ru>
2025-03-07 12:00:38 +0300

    ClickDetector теперь сохраняет через Awake _mainCamera

--- Assets/Scripts/ClickDetector.cs
+++ Assets/Scripts/ClickDetector.cs
@@ -3,14 +3,20 @@ using UnityEngine;
 public class ClickerDetected : MonoBehaviour
 {
 	[SerializeField] private LayerMask _layerMask;
+	private Camera _mainCamera;
 
 	public event System.Action<Cube> HittedCube;
+
+	private void Awake()
+	{
+		_mainCamera = Camera.main;
+	}
 
 	private void Update()
 	{
 		if (Input.GetMouseButtonDown(0))
 		{
-			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			Ray ray = _mainCamera.ScreenPointToRay(Input.mousePosition);
 			RaycastHit hit;
 
 			if (Physics.Raycast(ray, out hit, Mathf.Infinity, _layerMask))
